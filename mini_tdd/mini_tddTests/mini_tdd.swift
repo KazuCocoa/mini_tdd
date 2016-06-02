8080fc9eb8afaedfc462b1fede1fbc2e14f14e2a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-06-03 00:46:40 +0900

    fix tdd cases

--- mini_tdd/mini_tddTests/mini_tdd.swift
+++ mini_tdd/mini_tddTests/mini_tdd.swift
@@ -1,8 +1,16 @@
+enum CloseRangeError: ErrorType {
+    case ArgumentError
+}
+
 struct ClosedRange {
     let begin: Int
     let end: Int
 
-    init(begin b_int: Int, end e_int: Int) {
+    init(begin b_int: Int, end e_int: Int) throws {
+        if b_int > e_int {
+            throw CloseRangeError.ArgumentError
+        }
+
         self.begin = b_int
         self.end = e_int
     }
@@ -15,5 +23,21 @@ struct ClosedRange {
 extension ClosedRange {
     func toString() -> String {
         return "[\(begin),\(end)]"
+    }
+}
+
+extension ClosedRange {
+    func equals(compared: ClosedRange) -> Bool {
+        return (begin == compared.begin) && (end == compared.end)
+    }
+}
+
+extension ClosedRange {
+    func contains(compared: Int) -> Bool {
+        return (begin <= compared) && (compared <= end)
+    }
+
+    func contains(compared: ClosedRange) -> Bool {
+        return (begin <= compared.begin) && (end >= compared.end)
     }
 }
--- mini_tdd/mini_tddTests/mini_tddTests.swift
+++ mini_tdd/mini_tddTests/mini_tddTests.swift
@@ -2,15 +2,54 @@ import XCTest
 
 class TddTest: XCTestCase {
     func testRangeBeginAndEnd() {
-        let closedRange = ClosedRange.init(begin: 3, end: 8)
+        let closedRange = try! ClosedRange.init(begin: 3, end: 8)
 
         XCTAssert(closedRange.begin == 3)
         XCTAssert(closedRange.end == 8)
     }
 
     func testRangeWithString() {
-        let closedRange = ClosedRange.init(begin: 3, end: 8)
+        let closedRange = try! ClosedRange.init(begin: 3, end: 8)
 
         XCTAssert(closedRange.toString() == "[3,8]")
+    }
+
+    func testRangeErrirCase() {
+        XCTAssertThrowsError(try ClosedRange.init(begin: 8, end: 3))
+    }
+
+    func testContainsInRange() {
+        let closedRange = try! ClosedRange.init(begin: 3, end: 8)
+        XCTAssertTrue(closedRange.contains(5))
+    }
+
+    func testNotContainsInRange() {
+        let closedRange = try! ClosedRange.init(begin: 3, end: 8)
+        XCTAssertFalse(closedRange.contains(-1))
+    }
+
+
+    func testSameRange() {
+        let range1 = try! ClosedRange.init(begin: 3, end: 8)
+        let range2 = try! ClosedRange.init(begin: 3, end: 8)
+        XCTAssertTrue(range1.equals(range2))
+    }
+
+    func testNotSameRange() {
+        let range1 = try! ClosedRange.init(begin: 3, end: 8)
+        let range2 = try! ClosedRange.init(begin: 1, end: 6)
+        XCTAssertFalse(range1.equals(range2))
+    }
+
+    func testContainesRange() {
+        let range1 = try! ClosedRange.init(begin: 3, end: 8)
+        let range2 = try! ClosedRange.init(begin: 4, end: 6)
+        XCTAssertTrue(range1.contains(range2))
+    }
+
+    func testNotContainesRange() {
+        let range1 = try! ClosedRange.init(begin: 3, end: 8)
+        let range2 = try! ClosedRange.init(begin: 1, end: 10)
+        XCTAssertFalse(range1.contains(range2))
     }
 }
